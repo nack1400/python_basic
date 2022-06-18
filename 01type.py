c14919bab853371d4b379e5ed419106a8ff9c2fc
nack1400 <nack1400@naver.com>
2022-06-18 11:07:28 +0900

    first commit

---
+++ 01type.py
@@ -0,0 +1,7 @@
+a_string = 'like this' # 따옴표나 쌍따옴표 안에 문자열
+a_number = 3 
+a_float = 3.12 # float형 소수
+a_boolean = False # 첫자는 대문자로
+a_none = None # 비어있음
+
+print(type(a_string)) # 타입 확인
---
+++ 02list.py
@@ -0,0 +1,9 @@
+days = ["Mon","Tue","Wed","Thur","Fri"]
+print("Mon" in days) # days안에 Mon이 있냐
+print(days[2]) # days의 세번째 항은?
+
+print(days)
+days.append("Sat") # "Sat"을 추가
+print(days)
+days.reverse() # 순서 뒤집기
+print(days)
