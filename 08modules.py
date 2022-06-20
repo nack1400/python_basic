ac056ae487029abf867b298c1370518285fcba73
nack1400 <nack1400@naver.com>
2022-06-21 00:04:29 +0900

    Add : modules

---
+++ 08modules.py
@@ -0,0 +1,11 @@
+# import math
+from math import ceil, fsum
+from math import fsum as sexy_sum
+from calculator import plus, minus
+
+# print(math.ceil(1.2))
+# print(math.fabs(-1.2))
+print(ceil(1.2))
+print(fsum([1,2,3,4,5,6,7]))
+print(sexy_sum([1,2,3,4,5,6,7]))
+print(plus(1,2), minus(1,2))
