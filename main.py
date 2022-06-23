c2e76b44249e8803302915689898de4d35662e23
nack1400 <nack1400@naver.com>
2022-06-24 00:49:12 +0900

    Fix : variable name

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 pagination = indeed_soup.find("div", {"class": "pagination"})
 
-pages = pagination.find_all('a')
-spans = []
-for page in pages:
-    spans.append(page.find("span"))
-print(spans[:-1])
+links = pagination.find_all('a')
+pages = []
+for link in links[:-1]:
+    pages.append(int(link.find("span")))
+
+max_page = pages[-1]
