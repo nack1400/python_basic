513bae630d0577a81aad1d4b01c8a1cfa0af21d6
nack1400 <nack1400@naver.com>
2022-06-23 21:13:30 +0900

    Add : Indeed page

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-indeed_result= requests.get("https://search.indeed.jobs/main/jobs?keywords=python&location=&limit=50&page=1")
+indeed_result = requests.get(
+    "https://search.indeed.jobs/main/jobs?keywords=python&location=&limit=50&page=1")
 
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+pages = pagination.find_all('a')
+spans = []
+for page in pages:
+    spans.append(page.find("span"))
+print(spans[:-1])
