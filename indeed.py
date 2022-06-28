c27e9204a1954cf7843562faa71cce829ee0345b
nack1400 <nack1400@naver.com>
2022-06-29 00:13:56 +0900

    Add : location and finishing

--- indeed.py
+++ indeed.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://search.indeed.jobs/main/jobs?keywords=python&location=&limit={LIMIT}&page=1"
 
+
 def extract_indeed_pages():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
@@ -18,19 +19,33 @@ def extract_indeed_pages():
     max_page = pages[-1]
     return max_page
 
-def extract_indeed_jobs(last_page):
-  jobs = []
-  # for page in range(last_page):
-  result = requests.get(f"{URL}&start={0*LIMIT}")
-  soup = BeautifulSoup(result.text, "html.parser")
-  results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-  for result in results:
-    title = result.find("div", {"class": "title"}).find("a")["title"]
-    company = result.find("span", {"class": "company"})
+
+def extract_job(html):
+    title = html.find("div", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
     if company.find("a") is not None:
-      company = str(company.find("a").string)
+        company = str(company.find("a").string)
     else:
-      company = str(company.string)
+        company = str(company.string)
     company = company.strip()
-    print(title, company)
-  return jobs
+    location = html.find("span", {"class": "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {
+        'title': title,
+        'company': company, 
+        'location': location, 
+        "link": f"https://www.indeed.com/viewjob?jk={job_id}"
+    }
+
+
+def extract_indeed_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
