8fbb6bb1b0378d097fde895ea5199fc01b2fdb18
nack1400 <nack1400@naver.com>
2022-07-02 11:45:13 +0900

    Add : so extract jobs

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from so import get_jobs as get_so_jobs
 #indeed_jobs = get_indeed_jobs()
 so_jobs = get_so_jobs()
 
-print(indeed_jobs)
+print(so_jobs)
--- so.py
+++ so.py
@@ -1,3 +1,4 @@
+from sys import last_type
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,7 +9,18 @@ def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(soup.text, "html.parser")
     pages = soup.find("div", {"class": "pagination"}).find_all("a")
-    print(pages)
+    last_page = pages[-2].get_text(strip=True)
+    return last_page
+
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        result = soup.ffind_all("div", {"class": "-job"})
+        for result in results:
+          print(result["data-jobid"])
 
 
 def get_jobs():
