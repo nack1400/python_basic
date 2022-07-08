06795554dd0887717079485c0b7c1ef38d8ab070
nack1400 <nack1400@naver.com>
2022-07-09 00:02:40 +0900

    Add : practice oop

---
+++ Django/02oop.py
@@ -0,0 +1,14 @@
+class Car():
+  wheels = 4
+  doors = 4
+  windows = 4
+  seats = 4
+
+porche = Car()
+porche.color = "Red"
+
+ferrari = Car()
+ferrari.color = "Yellow"
+
+mini = Car()
+mini.color = "White"
