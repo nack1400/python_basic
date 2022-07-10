629508c4a8f96ac3350f33a1d442ba25152e55bc
nack1400 <nack1400@naver.com>
2022-07-11 00:36:42 +0900

    Add : practice method2

---
+++ Django/04method2.py
@@ -0,0 +1,27 @@
+class Car():
+    wheels = 4
+    doors = 4
+    windows = 4
+    seats = 4
+
+    # 오버라이딩
+    def __init__(self, **kwargs):
+        print(kwargs.get)
+        self.wheels = 4
+        self.doors = 4
+        self.windows = 4
+        self.seats = 4
+        self.color = kwargs.get("color", "black")
+        self.price = kwargs.get("price", "$20")
+
+    def __str__(self):
+        return f"Car with {self.wheels} wheels"
+
+# print(dir(Car))
+
+
+porche = Car(color="green", price="$40")
+print(porche.color, porche.price)
+
+mini = Car()
+print(mini.color, mini.price)
